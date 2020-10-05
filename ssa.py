fa59929afe68033166d64e1ea05a3014e267b791
Will Fatherley <wefatherley@gmail.com>
2020-10-05 15:33:51 -0400

    first-family struggle, and minor related changes

--- ssa.py
+++ ssa.py
@@ -10,9 +10,9 @@ class SSA:
         if a is not None or version is not None:
             seed(a=a, version=(version or 2))
 
-    def direct(self, model, count=1):
-        """Generator of direct-method trajectories"""
-        while count > 0:
+    def direct(self, model):
+        """Indefinite generator of direct-method trajectories"""
+        while True:
             while not model.exit():
 
                 # init step: reaction probabilties and partition func
@@ -40,9 +40,9 @@ class SSA:
             model.reset()
             count -= 1
 
-    def first_reaction(self, model, count=1):
-        """Generator of 1st-reaction trajectories"""
-        while count > 0:
+    def first_reaction(self, model):
+        """Indefinite generator of 1st-reaction trajectories"""
+        while True:
             while not model.exit():
 
                 # monte carlo step: generate reaction times
@@ -63,49 +63,75 @@ class SSA:
             model.reset()
             count -= 1
 
-    def first_family(self, model, count=1, families=1):
-        """Generator of 1st-family trajectories"""
-        while count > 0:
-            
-            # build list of families
-            families_list = list()
-            family_size = len(model.propen) // families
-            i = 0
-            for j in range(families):
-                if j < families - 1:
-                    families_list.append(
-                        (j, model.propen[i:i+family_size])
-                    )
-                else:
-                    families_list.append(
-                        (j, model.propen[i:i+family_size])
-                    )
+    def first_family(self, model, families=2):
+        """Indefinite generator of 1st-family trajectories"""
 
+        # build list of families
+        families_list = list()
+        family_size = len(model.propen) // families
+        i = 0
+        for j in range(families):
+            if j < families - 1:
+                families_list.append(
+                    (j, model.propen[i:i+family_size])
+                )
+            else:
+                families_list.append(
+                    (j, model.propen[i:i+family_size])
+                )
+
+        while True:
             while not model.exit():
-                
+
             yield model.trajectory
             model.reset()
             count -= 1
 
 
 
-class Model(dict):
-    """
-    WIP
-    - self is a dict of N species and a temporal param
-    - self.stoich is a dict of M stoichiometry lists
-    - self.propen is dict of M propensity lambdas
-    """
-    def __init__(self, initia, propen, stoich):
-        self.propen = propen.items().sort(
-            key=lambda t: t[0], reverse=True
+class Epidemic(dict):
+    """Simple model to meet design of class SSA"""
+    def __init__(
+        self,
+        initial_conditions: dict,
+        propensities: dict,
+        stoichiometry: dict
+    ):
+        self.propen = sorted(
+            propensities.items(), reverse=True
         )
-        self.stoich = stoich.items()
-        super().__init__(**initia)
-
+        self.stoich = list(stoichiometry.items())
+        super().__init__(**initial_conditions)
+        
     def exit(self):
         pass
         
     def reset(self):
         for key in self.keys():
             del self[key][1:]
+
+
+def first_family(model, families=2):
+    """Indefinite generator of 1st-family trajectories"""
+    # build list of families
+    families_list = list()
+    family_size = len(model.propen) // families
+    i = 0
+    for j in range(families):
+        if j == families - 1:
+            family_size += len(model.propen) % families
+        families_list.append(
+            (j, model.propen[i:i+family_size])
+        )
+        i += family_size
+        return families_list
+        
+ic = dict(s=99,i=1,r=0)
+p = {
+    0: lambda d: 0.5 * d["s"] * d["i"] / sum(d.values()),
+    1: lambda d: 1.0 * d["i"],
+}
+s = {
+    0: {"s": -1, "i": 1, "r": 0},
+    1: {"s": 0, "i": -1, "r": 1}
+}
